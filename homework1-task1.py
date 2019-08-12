9a38db65ccfb11c7f89063335bc100d1b47ccec1
silencer-terminated <vasilyk@yandex.ru>
2019-08-12 20:45:09 +0200

    Delete homework1-task1.py

--- homework1-task1.py
+++
@@ -1,6 +0,0 @@
-long_phrase = "Насколько проще было бы писать программы, если бы не заказчики"
-short_phrase = "640Кб должно хватить для любых задач. Билл Гейтс (по легенде)"
-if len(long_phrase) > len(short_phrase):
-    print ("Ja-ja! Das ist TRUE!")
-else:
-    print ("Neeeein! Das ist FALSE!")
